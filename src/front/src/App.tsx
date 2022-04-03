9e0e115a4df0245d119d50e90309b3c879e4dc18
Bartek <obudzinskib@gmail.com>
2022-04-03 14:29:25 +0200

    feat: added front

---
+++ src/front/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import React, { Suspense } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <Button type="primary" size="large">
+      test
+    </Button>
+  );
+};
+
+export default App;
